1b55cf6bc536bc3aec8cc492843399813768efec
Ben Parsell <parsellb@mail.gvsu.edu>
2023-04-22 22:56:13 -0400

    lecture 190

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { User } from './models/User';
 
-const user = new User({ id: 1 });
+const user = User.buildUser({ id: 1, name: 'benny', age:26 });
 
 user.on('change', () => {
   console.log(user)
