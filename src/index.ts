552c43939caf9ce98d37fd69f78153fd17c76658
Ben Parsell <ben.parsell1507@gmail.com>
2023-04-27 22:31:19 -0400

    Final lecture

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
-import { UserForm } from "./views/UserForm";
-import { User } from "./models/User";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { UserProps, User } from "./models/User";
 
-const user = User.buildUser({name: 'Name', age: 21})
+const users = new Collection('http://localhost:3000/users', (json: UserProps) => {
+  return User.buildUser(json);
+})
+users.on('change', () => {
+  const root = document.getElementById('root');
 
-const root = document.getElementById('root');
+  if(root) {
+    new UserList(root, users).render();
+  }
+});
 
-
-if(root) {
-   const userForm = new UserForm(
-      root,
-      user
-   );
-   userForm.render();
-} else {
-   throw new Error('Root element not found')
-}
-
-
+users.fetch();
